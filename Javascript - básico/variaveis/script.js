4b09e1c8a7e30840b5e9c8013009b8d2188935b2
Bruno Andreotti <brunoandreotti@outlook.com>
2021-03-02 17:37:38 -0300

    variaveis

---
+++ Javascript - básico/variaveis/script.js
@@ -0,0 +1,18 @@
+var a = "Placa de Captura";
+
+var b;
+
+b = "HD";
+
+b = a
+
+
+
+// Uma variável pode estar dentro de outra.
+// A ordem no JS é muito importante, se nesse caso o "b = a" estivesse encima do "b = HD", a variável b teria o valor "HD" pois está depois do "b = a"
+
+
+
+
+console.log(a);
+console.log(b);
