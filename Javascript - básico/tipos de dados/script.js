7d124238a55b6c29df76b3d46840f805aaded740
Bruno Andreotti <brunoandreotti@outlook.com>
2021-03-15 15:12:51 -0300

    Tipos de dados

---
+++ Javascript - básico/tipos de dados/script.js
@@ -0,0 +1,33 @@
+/*
+Boolean
+Null
+Undefined
+Number
+String
+
+*/
+
+// String e Number
+
+var a = "1";
+var b = 1;
+
+console.log(a + a)
+console.log( b + b)
+
+/* String é uma cadeia de caracteres, não possui valor de números, já o Number possui o valor do número.
+
+No caso a cima, ao somar as variáveis, apenas a variável b apresenta soma, a variável a apresenta concatenação, por ser uma String */ 
+
+// Undefined e Null
+
+/*
+Undefined équando a variavelnão é iniciada ou não há nada nela, o programa processa como undefined pois não colocamos nada de valor ou dado
+Enquanto o Null, semelhante ao Undefined, não há dado, estamos dando a certeza para o programa que o valor é vazio ou null.
+
+*/
+
+// Boolean
+/*
+Possui valor true ou false
+*/
