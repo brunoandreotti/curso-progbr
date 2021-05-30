0cb7928b28ee04e7bf727dc0a55b79f0d8e432d4
Bruno Andreotti <brunoandreotti@outlook.com>
2021-05-30 19:05:22 -0300

    Operadores lógicos

---
+++ Javascript - básico/operadores lógicos/script.js
@@ -0,0 +1,53 @@
+// && e
+// || ou
+// Negativo
+
+// var a = true;
+// var b = false;
+
+// var c = a && b;
+
+// console.log(c)
+
+// var idade = 25;
+// var maior20 = idade > 20;
+// var menor30 = idade < 30;
+
+// console.log("Qual a idade?", idade)
+
+// console.log("Maior que vinte?", maior20);
+
+// console.log("Menor que trinta?", menor30);
+
+// var entre = maior20 && menor30
+
+// console.log("Entre 20 e 30?", entre);
+
+
+
+// Programa para ver gratuidade na entrega para 10 anos ou menos ou 60 anos ou mais
+
+// var idade = 10;
+// var menor10 = idade <= 10;
+// var maior60 =  idade >= 60;
+
+// var desconto = menor10 || maior60;
+
+// console.log("Qual idade?", idade);
+// console.log("Menor ou igual a 10 anos?", menor10);
+// console.log("Maior ou igual a 60 anos?", maior60);
+// console.log("Tem gratuidade?", desconto);
+
+
+// Negativo
+
+var idade = 25
+
+var maior20 = idade >= 20;
+var menor20 = idade < 20;
+
+console.log("Maior que vinte?", maior20);
+console.log("Menor que 20?", menor20)
+
+
+
