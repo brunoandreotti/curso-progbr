1fadb3bda4ee3e41d3a11af9e713776bcfee4b00
Bruno Andreotti <brunoandreotti@outlook.com>
2021-05-30 19:55:38 -0300

    Alert e prompt

---
+++ Javascript - básico/alert e prompt/script.js
@@ -0,0 +1,17 @@
+// alert("Bom dia, Bruno")
+
+// var nome = prompt("Qual seu nome?")
+
+// alert("Bom dia, " + nome)
+
+// var numero = prompt("Digite um número")
+// var dobro = parseInt(numero) + parseInt(numero)
+
+// alert("O número que você digitou é " + numero)
+// alert("O dobro de " + numero + " é " + dobro)
+
+
+var ano = prompt("Qual ano você nasceu?")
+var idade = 2021 - ano;
+
+alert("Sua idade é " + idade)
