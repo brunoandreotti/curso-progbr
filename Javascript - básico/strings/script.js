85f79eb3c5a4405d46cdb9c08e4279b3f68d08a8
Bruno Andreotti <brunoandreotti@outlook.com>
2021-05-30 19:31:36 -0300

    Transformando strings em números

---
+++ Javascript - básico/strings/script.js
@@ -0,0 +1,24 @@
+
+
+
+
+// var string = "Qualquer coisa " + " Outra coisa"
+// // string += " outra coisa"
+
+// console.log(string)
+
+
+// Transformando strings em números
+
+
+var a = "5.5";
+var b = "3.3";
+
+var c = a + b;
+var d = parseInt(a) + parseInt(b)
+var e = parseFloat(a) + parseFloat(b)
+
+console.log(c)
+console.log(d)
+console.log(e)
+
